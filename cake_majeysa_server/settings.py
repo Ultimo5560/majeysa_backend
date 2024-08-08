50870d23eb3372e0844371315207d1b1472bf04f
Eber <eber926@hotmail.com>
2024-08-08 00:23:01 -0600

    Error origin solicionado

--- cake_majeysa_server/settings.py
+++ cake_majeysa_server/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://pasteleriamajeysa.netlify.app"
+    "https://pasteleriamajeysa.netlify.app",
 ]
 
 #MAIL_SETTINGS
